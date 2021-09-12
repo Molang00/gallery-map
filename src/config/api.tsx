4a0a38abe767a38b1403bdcd918f90014e1bbcbe
byeongsu kim <bskim@autocrypt.io>
2021-09-13 01:15:34 +0900

    update image base url for develop

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -1,5 +1,7 @@
 // export const baseUrl = 'http://192.168.35.107:3030';
-export const baseUrl = 'http://3.38.43.164:3030';
+// export const imageBaseUrl =
+// 'https://dev-gallery-map.s3.ap-northeast-2.amazonaws.com/image/';
 
+export const baseUrl = 'http://3.38.43.164:3030';
 export const imageBaseUrl =
   'https://gallery-map.s3.ap-northeast-2.amazonaws.com/image/';
